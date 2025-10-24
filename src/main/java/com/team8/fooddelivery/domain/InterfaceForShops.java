9da5d9b5913822bddb576501edb841c1b7356c2f
Dmitry Bobryakov <32440472+DmitryBobryakov@users.noreply.github.com>
2025-10-24 16:33:10 +0300

    Update InterfaceForShops.java

--- src/main/java/com/team8/fooddelivery/domain/InterfaceForShops.java
+++ src/main/java/com/team8/fooddelivery/domain/InterfaceForShops.java
@@ -2,25 +2,25 @@ package com.team8.fooddelivery.domain;
 
 import java.util.List;
 
-public interface InterfaceForShops {
+public interface InterfaceForShops { // ShopsInfoService, ShopsProductsService, ShopsOrderService + разделить на 3 интерфейса
   
-  Shops registerShop(Shops infoAbout);
+  Shops registerShop(Shops infoAbout); // + набор полей
 
-  void approveShop(Long shopId);
+  void approveShop(Long shopId); // возвращать статус
 
-  void rejectShop(String rejectionReason);
+  void rejectShop(String rejectionReason); // возвращать статус
 
-  String changePassword(Long shopId, String emailForAU, String phoneForAU);
+  String changePassword(Long shopId, String emailForAU, String phoneForAU); // + new password
 
-  String changeEmailForAU(Long shopId, String phoneForAU, String password);
+  String changeEmailForAU(Long shopId, String phoneForAU, String password); // + new email
 
-  String changePhoneForAU(Long shopId, String emailForAU, String password);
+  String changePhoneForAU(Long shopId, String emailForAU, String password); // + new phone
 
-  //Shops getShopById(Long shopId);
+  Shops getShopById(Long shopId);
 
-  Shops updateShopInfo(Long shopId);
+  Shops updateShopInfo(Long shopId); // + new Shop
 
-  Product addProduct(Long shopId);
+  Product addProduct(Long shopId); // + набор полей
 
   Product updateProduct(Long shopId, Integer productId);
 
