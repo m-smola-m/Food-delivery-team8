427322f291e151c28ebc3ac73d43f2bbc7f9e8da
Dmitry Bobryakov <32440472+DmitryBobryakov@users.noreply.github.com>
2025-10-24 16:26:14 +0300

    Update Order.java

--- src/main/java/com/team8/fooddelivery/domain/Order.java
+++ src/main/java/com/team8/fooddelivery/domain/Order.java
@@ -9,22 +9,22 @@ import lombok.*;
 public class Order {
   private Long orderId;
   private Long shopId;
-  private String customerName;
-  private String customerPhone;
+  private String customerName; // Long clientId
+  private String customerPhone; // delete
   private List<Product> items;
   private Address deliveryAddress;
   private StoreOrderStatus status;
   private double totalAmount;
   private double storeRevenue;
   private LocalDateTime orderTime;
-  private LocalDateTime estimatedReadyTime;
+  private LocalDateTime estimatedReadyTime;  // Instant
   private LocalDateTime actualReadyTime;
   private String customerNotes;
   private String internalNotes;
   private String rejectionReason;
-  private Integer preparationTime;
+  private Integer preparationTime;   // Instant
   private boolean isPaid;
-  private String paymentMethod;
+  private String paymentMethod; // enum
 
   public enum StoreOrderStatus {
     PENDING, CONFIRMED, PREPARING, READY, PICKED_UP, COMPLETED, REJECTED, CANCELLED
