ca4e4b4e09b663137149ac6e7d2dd5d415abb075
nachynondar <ondar.nb@phystech.edu>
2025-11-11 07:31:17 -0800

    Update CourierWorkService.java

--- src/main/java/com/team8/fooddelivery/service/CourierWorkService.java
+++ src/main/java/com/team8/fooddelivery/service/CourierWorkService.java
@@ -1,12 +1,13 @@
 package com.team8.fooddelivery.service;
 
+import com.team8.fooddelivery.model.Order;
 import java.util.List;
 
 public interface CourierWorkService {
-  boolean acceptOrder(Long courierId, Long orderId);
-  void pickupOrder(Long courierId, Long orderId);
-  void completeOrder(Long courierId, Long orderId);
-  List<String> getOrderHistory(Long courierId);
-  void startShift(Long courierId);
-  void endShift(Long courierId);
+  boolean acceptOrder(Long courierId, Long orderId);
+  void pickupOrder(Long courierId, Long orderId);
+  void completeOrder(Long courierId, Long orderId);
+  List<Order> getOrderHistory(Long courierId);
+  void startShift(Long courierId);
+  void endShift(Long courierId);
 }
