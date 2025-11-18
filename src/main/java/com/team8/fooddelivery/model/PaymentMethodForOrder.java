266ac5fd7c468da88d3d0f98acb6fc1c7dfefcd0
Смолева Наталия Сергеевна <stephen.hoffman.143@gmail.com>
2025-11-18 15:20:00 +0300

    Revert "реализация методов со стороны ресторанов"

--- src/main/java/com/team8/fooddelivery/model/PaymentMethodForOrder.java
+++
@@ -1,5 +0,0 @@
-package com.team8.fooddelivery.model;
-
-public enum PaymentMethodForOrder {
-  YO
-}
--- src/main/java/com/team8/fooddelivery/model/ProductCategory.java
+++
@@ -1,5 +0,0 @@
-package com.team8.fooddelivery.model;
-
-public enum ProductCategory {
-  BAKERY,  MAIN_DISH, DESSERT, DRINK, OTHER
-}
