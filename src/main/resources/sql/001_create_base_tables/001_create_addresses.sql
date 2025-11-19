4f6d7020350f16cbcc2760d46def55ac8da26571
n-smola-n <nataliiasmoleva2@yandex.ru>
2025-11-19 20:05:46 +0300

    все работает

--- src/main/resources/sql/001_create_base_tables/001_create_addresses.sql
+++ src/main/resources/sql/001_create_base_tables/001_create_addresses.sql
@@ -1,11 +1,17 @@
 -- Таблица адресов
 CREATE TABLE addresses (
-                           id BIGSERIAL PRIMARY KEY,
-                           street VARCHAR(255) NOT NULL,
-                           city VARCHAR(100) NOT NULL,
-                           postal_code VARCHAR(20),
-                           country VARCHAR(100) DEFAULT 'Russia',
-                           latitude DOUBLE PRECISION,
-                           longitude DOUBLE PRECISION,
-                           created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+    id BIGSERIAL PRIMARY KEY,
+    country VARCHAR(100) DEFAULT 'Russia',
+    city VARCHAR(100) NOT NULL,
+    street VARCHAR(255),
+    building VARCHAR(20),
+    apartment VARCHAR(20),
+    entrance VARCHAR(10),
+    floor INTEGER,
+    postal_code VARCHAR(20),
+    latitude DOUBLE PRECISION,
+    longitude DOUBLE PRECISION,
+    address_note TEXT,
+    district VARCHAR(100),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
