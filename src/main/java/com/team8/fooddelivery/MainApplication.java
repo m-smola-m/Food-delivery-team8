2fe82ed4e7789f9b7b7bb6fd5e75aae28205e912
n-smola-n <nataliiasmoleva2@yandex.ru>
2025-11-30 23:36:58 +0300

    подчистила файлы

--- src/main/java/com/team8/fooddelivery/MainApplication.java
+++ src/main/java/com/team8/fooddelivery/MainApplication.java
@@ -1,6 +1,12 @@
 package com.team8.fooddelivery;
 
 import com.team8.fooddelivery.util.DatabaseInitializer;
+import com.team8.fooddelivery.util.WebAppExtractor;
+import org.apache.catalina.Context;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.core.StandardContext;
+
+import java.nio.file.Path;
 
 /**
  * Главный класс приложения для запуска инициализации базы данных
@@ -13,8 +19,22 @@ public class MainApplication {
         try {
             DatabaseInitializer.initializeDatabase();
             System.out.println("🎉 Инициализация базы данных завершена успешно!");
+
+            int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "8080"));
+            Path webAppDir = WebAppExtractor.extractWebAppToTemp();
+
+            Tomcat tomcat = new Tomcat();
+            tomcat.setPort(port);
+            tomcat.getConnector();
+
+            Context context = tomcat.addWebapp("", webAppDir.toString());
+            ((StandardContext) context).setReloadable(false);
+
+            tomcat.start();
+            System.out.printf("🌐 Приложение запущено на http://localhost:%d/%n", port);
+            tomcat.getServer().await();
         } catch (Exception e) {
-            System.err.println("💥 Ошибка при инициализации базы данных: " + e.getMessage());
+            System.err.println("💥 Ошибка при запуске приложения: " + e.getMessage());
             e.printStackTrace();
             System.exit(1);
         }
